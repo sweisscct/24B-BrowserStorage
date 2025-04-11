25f2f5e1803f7be063bb05f95c0d755885b89a2f
sweisscct <114678287+sweisscct@users.noreply.github.com>
2025-04-11 13:40:48 +0100

    Persist background colour change

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ let submitColourButton = document.getElementById("submit-colour-change");
 let colourSelector = document.getElementById("background-colour-selector");
 let body = document.getElementsByTagName("body")[0];
 
+body.style.backgroundColor = localStorage.getItem("background-colour");
+
 
 submitColourButton.addEventListener("click", () => {
     let newColour = colourSelector.value;
     body.style.backgroundColor = newColour;
+
+    localStorage.setItem("background-colour", newColour);
 });
+
+/*
+    Exercise
+    Include an input that will change the text size on screen
+    Persist this setting
+*/
