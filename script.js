09cd6d59db5be59a83f58771ed029e6ebd924e00
sweisscct <114678287+sweisscct@users.noreply.github.com>
2025-04-11 14:28:14 +0100

    font size change

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let submitColourButton = document.getElementById("submit-colour-change");
 let colourSelector = document.getElementById("background-colour-selector");
 let body = document.getElementsByTagName("body")[0];
+let submitFontButton = document.getElementById("submit-font-size-change");
+let fontSizeSelector = document.getElementById("font-size-selector");
 
 body.style.backgroundColor = localStorage.getItem("background-colour");
-
+body.style.fontSize = localStorage.getItem("font-size");
 
 submitColourButton.addEventListener("click", () => {
     let newColour = colourSelector.value;
@@ -11,6 +13,14 @@ submitColourButton.addEventListener("click", () => {
 
     localStorage.setItem("background-colour", newColour);
 });
+
+submitFontButton.addEventListener("click", () => {
+    let newSize = fontSizeSelector.value+"px";
+    console.log(newSize);
+    body.style.fontSize = newSize;
+
+    localStorage.setItem("font-size", newSize);
+})
 
 /*
     Exercise
